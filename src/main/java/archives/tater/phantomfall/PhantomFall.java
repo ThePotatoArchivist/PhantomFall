8f3cfb97b296fbf51a0199185315de2f4b79f712
JosiahFu <josiahfu@gmail.com>
2025-07-14 22:28:42 -0700

    Add spawning mechanic

--- src/main/java/archives/tater/phantomfall/PhantomFall.java
+++ src/main/java/archives/tater/phantomfall/PhantomFall.java
@@ -2,18 +2,32 @@ package archives.tater.phantomfall;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.EntityElytraEvents;
+import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
 import net.fabricmc.fabric.api.entity.event.v1.FabricElytraItem;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.damage.DamageType;
 import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ElytraItem;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.SpawnHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static net.minecraft.util.math.MathHelper.clamp;
 
 public class PhantomFall implements ModInitializer {
 	public static final String MOD_ID = "phantomfall";
@@ -34,6 +48,17 @@ public class PhantomFall implements ModInitializer {
 		return !(chestEquipment instanceof ElytraItem) && !(chestEquipment instanceof FabricElytraItem);
 	}
 
+	public static List<Integer> distributeSizes(int value, Random random) {
+		var sizes = new ArrayList<Integer>();
+		var remaining = value;
+		while (remaining > 0) {
+			var size = max(min(random.nextBetween(2, 5), remaining), 2);
+			sizes.add(size - 1); // Phantom size starts at 0
+			remaining -= size;
+		}
+		return sizes;
+	}
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -43,6 +68,8 @@ public class PhantomFall implements ModInitializer {
             if (!source.isIn(PHANTOM_PICKUP)) return;
 			var attacker = source.getAttacker();
 			if (!(attacker instanceof PhantomEntity)) return;
+			if (entity.getVehicle() instanceof PhantomEntity) return;
+			if ((entity instanceof PlayerEntity) && PhantomBodyComponent.KEY.get(entity).getPhantom() != null) return;
             entity.startRiding(attacker);
         });
 		ServerLivingEntityEvents.ALLOW_DEATH.register((entity, damageSource, damageAmount) -> {
@@ -61,5 +88,37 @@ public class PhantomFall implements ModInitializer {
 		});
 		EntityElytraEvents.CUSTOM.register((entity, tickElytra) ->
 				entity instanceof PlayerEntity player && PhantomBodyComponent.KEY.get(player).getPhantom() != null);
+
+		EntitySleepEvents.STOP_SLEEPING.register((entity, sleepingPos) -> {
+			var world = entity.getWorld();
+			if (!(world instanceof ServerWorld serverWorld)) return;
+			if (!(entity instanceof PlayerEntity player)) return;
+			var phantomBodyComponent = PhantomBodyComponent.KEY.get(player);
+			world.calculateAmbientDarkness();
+			if (!world.isNight()) {
+				phantomBodyComponent.resetSpawnedPhantoms();
+				return;
+			}
+			if (!world.isSkyVisible(entity.getBlockPos())) return;
+
+			var spawnedPhantoms = phantomBodyComponent.getSpawnedPhantoms();
+			var random = world.getRandom();
+
+			if (random.nextFloat() > 0.5f + 0.05f * spawnedPhantoms) return;
+
+			player.playSoundToPlayer(SoundEvents.ENTITY_PHANTOM_AMBIENT, player.getSoundCategory(), 1f, 0.6f);
+
+			for (var size : distributeSizes(clamp(spawnedPhantoms, 1, 16), random)) {
+				var blockPos = entity.getBlockPos().up(20 + random.nextInt(15)).east(-10 + random.nextInt(21)).south(-10 + random.nextInt(21));
+				if (!SpawnHelper.isClearForSpawn(world, blockPos, world.getBlockState(blockPos), world.getFluidState(blockPos), EntityType.PHANTOM)) continue;
+				var phantom = EntityType.PHANTOM.create(world);
+				if (phantom == null) continue;
+				phantom.refreshPositionAndAngles(blockPos, 0.0F, 0.0F);
+				phantom.initialize(serverWorld, world.getLocalDifficulty(entity.getBlockPos()), SpawnReason.NATURAL, null);
+				phantom.setPhantomSize(size);
+				serverWorld.spawnEntityAndPassengers(phantom);
+				phantomBodyComponent.increaseSpawnedPhantoms();
+			}
+		});
 	}
 }
--- src/main/java/archives/tater/phantomfall/mixin/PhantomEntityMixin.java
+++ src/main/java/archives/tater/phantomfall/mixin/PhantomEntityMixin.java
@@ -2,15 +2,23 @@ package archives.tater.phantomfall.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.FlyingEntity;
 import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static java.lang.Math.max;
 
 @Mixin(PhantomEntity.class)
 public abstract class PhantomEntityMixin extends FlyingEntity {
+	@Shadow public abstract int getPhantomSize();
+
 	protected PhantomEntityMixin(EntityType<? extends FlyingEntity> entityType, World world) {
 		super(entityType, world);
 	}
@@ -27,7 +35,16 @@ public abstract class PhantomEntityMixin extends FlyingEntity {
 			method = "onSizeChanged",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/PhantomEntity;getPhantomSize()I")
 	)
-	private int preventDamageIncrease(int original) {
-		return 0;
+	private int reduceDamageIncrease(int original) {
+		return max(original - 1, 0);
+	}
+
+	@SuppressWarnings("DataFlowIssue")
+    @Inject(
+			method = "onSizeChanged",
+			at = @At("TAIL")
+	)
+	private void increaseHealth(CallbackInfo ci) {
+		getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(16 + 4 * getPhantomSize());
 	}
 }
