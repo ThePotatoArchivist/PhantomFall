102205468e7d0eeb36acbe20608c00febea79a25
JosiahFu <josiahfu@gmail.com>
2025-07-17 01:33:00 -0700

    Add config limits

--- src/main/java/archives/tater/phantomfall/PhantomFallConfig.java
+++ src/main/java/archives/tater/phantomfall/PhantomFallConfig.java
@@ -1,9 +1,7 @@
 package archives.tater.phantomfall;
 
 import folk.sisby.kaleido.api.WrappedConfig;
-import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
-import folk.sisby.kaleido.lib.quiltconfig.api.annotations.DisplayNameConvention;
-import folk.sisby.kaleido.lib.quiltconfig.api.annotations.SerializedNameConvention;
+import folk.sisby.kaleido.lib.quiltconfig.api.annotations.*;
 import folk.sisby.kaleido.lib.quiltconfig.api.metadata.NamingSchemes;
 
 @DisplayNameConvention(NamingSchemes.SPACE_SEPARATED_LOWER_CASE_INITIAL_UPPER_CASE)
@@ -19,12 +17,16 @@ public class PhantomFallConfig extends WrappedConfig {
 
     public static class Server implements Section {
         @Comment("Largest phantom size that can spawn naturally")
+        @IntegerRange(min = 1, max = 16)
         public int maxPhantomSize = 4;
         @Comment("Chance of phantoms spawning when a player is on 0 previous spawns")
+        @FloatRange(min = 0f, max = 1f)
         public float baseSpawnChance = 0.5f;
         @Comment("How much the spawn chance should increase per previous spawn")
+        @FloatRange(min = 0f, max = 1f)
         public float spawnChanceIncrease = 0.05f;
         @Comment("The cap for the \"spawning score\" used to spawn a group of phantoms")
+        @IntegerRange(min = 1, max = 64)
         public int maxSpawnScore = 8;
     }
 }
