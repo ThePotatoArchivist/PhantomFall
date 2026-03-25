f6119d9828bbac618e398888a0fb0885583399e6
ThePotatoArchivist <josiahfu@gmail.com>
2026-03-25 11:34:38 -0700

    Reduce base damage

--- src/main/java/archives/tater/phantomfall/mixin/PhantomEntityMixin.java
+++ src/main/java/archives/tater/phantomfall/mixin/PhantomEntityMixin.java
@@ -1,17 +1,18 @@
 package archives.tater.phantomfall.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Phantom;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Phantom;
+import net.minecraft.world.level.Level;
 
 import static java.lang.Math.max;
 
@@ -29,6 +30,14 @@ public abstract class PhantomEntityMixin extends Mob {
 	)
 	private int defaultSize(int size) {
 		return size == 0 ? 1 : size;
+	}
+
+	@ModifyExpressionValue(
+			method = "updatePhantomSizeInfo",
+			at = @At(value = "CONSTANT", args = "intValue=6")
+	)
+	private int reduceBaseDamage(int original) {
+		return original / 2;
 	}
 
 	@ModifyExpressionValue(
